4b79fcb4a11f4ea75fca3ab8ebad0383fe5c3a87
Sulemam-Ba <patricio_gato@hotmail.com>
2021-09-27 04:27:28 -0300

    Criação da app
    Calculadora

---
+++ Unifacs Notas/Unifacs Notas/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace Unifacs_Notas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mostramedia = new System.Windows.Forms.Label();
+            this.lblmedia = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CALCULAR = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mostramedia
+            // 
+            this.mostramedia.AutoSize = true;
+            this.mostramedia.Location = new System.Drawing.Point(272, 212);
+            this.mostramedia.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.mostramedia.Name = "mostramedia";
+            this.mostramedia.Size = new System.Drawing.Size(73, 13);
+            this.mostramedia.TabIndex = 23;
+            this.mostramedia.Text = "RESULTADO";
+            // 
+            // lblmedia
+            // 
+            this.lblmedia.AutoSize = true;
+            this.lblmedia.Location = new System.Drawing.Point(89, 302);
+            this.lblmedia.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblmedia.Name = "lblmedia";
+            this.lblmedia.Size = new System.Drawing.Size(0, 13);
+            this.lblmedia.TabIndex = 22;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(86, 294);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(148, 20);
+            this.textBox4.TabIndex = 21;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(86, 252);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(148, 20);
+            this.textBox3.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(86, 212);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 20);
+            this.textBox2.TabIndex = 19;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 172);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 299);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "NOTA 4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 259);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "NOTA 3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 219);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "NOTA 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 177);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "NOTA 1";
+            // 
+            // CALCULAR
+            // 
+            this.CALCULAR.Location = new System.Drawing.Point(255, 237);
+            this.CALCULAR.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.CALCULAR.Name = "CALCULAR";
+            this.CALCULAR.Size = new System.Drawing.Size(112, 35);
+            this.CALCULAR.TabIndex = 13;
+            this.CALCULAR.Text = "CALCULAR";
+            this.CALCULAR.UseVisualStyleBackColor = true;
+            this.CALCULAR.Click += new System.EventHandler(this.CALCULAR_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 141);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(144, 13);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "CALCULADORA DE NOTAS";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Unifacs_Notas.Properties.Resources.UNIFACS_MARCA_2019_colorida;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(285, 89);
+            this.pictureBox1.TabIndex = 26;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 331);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.mostramedia);
+            this.Controls.Add(this.lblmedia);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CALCULAR);
+            this.Name = "Form1";
+            this.Text = "UNIFACS NOTAS";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label mostramedia;
+        private System.Windows.Forms.Label lblmedia;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CALCULAR;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Unifacs Notas/Unifacs Notas/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unifacs_Notas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CALCULAR_Click(object sender, EventArgs e)
+        {
+            //DECLARAÇÃO DE VARIÁVEIS, DETERMINANDO SEU TIPO DOUBLE
+            double n1 = 0, n2 = 0, n3 = 0, n4 = 0, media = 0;
+
+            //CONVERTER STRING DA CAIXAS DE TEXTO PARA O TIPO DOUBLE
+            n1 = double.Parse(textBox1.Text);
+            n2 = double.Parse(textBox2.Text);
+            n3 = double.Parse(textBox3.Text);
+            n4 = double.Parse(textBox4.Text);
+
+            //CALCULA AS 4 NOTAS INFORMADAS PELO USUÁRIO
+            media = (n1 + n2 + n3 + n4) / 4;
+            mostramedia.Text = Convert.ToString(media);
+        }
+    }
+}
